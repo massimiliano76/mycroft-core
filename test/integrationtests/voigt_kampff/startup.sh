b2cc089f55f9212882b2c5350e60e7977648d2d1
Åke Forslund <ake.forslund@gmail.com>
2020-01-29 22:30:18 +0100

    Reset things stored in ~/.mycroft

--- test/integrationtests/voigt_kampff/startup.sh
+++ test/integrationtests/voigt_kampff/startup.sh
@@ -4,4 +4,13 @@ source /opt/mycroft/mycroft-core/.venv/bin/activate
 python -m test.integrationtests.voigt_kampff.test_setup -c ~/.mycroft/test.yml
 cd test/integrationtests/voigt_kampff/
 behave
+RESULT=$?
+
+# Remove temporary skill files
+rm -rf ~/.mycroft/skills
+# Remove intent cache
+rm -rf ~/.mycroft/intent_cache
+
+exit $RESULT
+
 #TODO Export logs
